137b1fa8d86e4f571e9698ec7c04c1d68d8b397e
TK.E.Project <tke.project@outlook.jp>
2024-01-08 22:32:43 +0900

    プロジェクト ファイルを追加します。

---
+++ MeviyImporter/Data/MeviyImporterDBContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MeviyImporter.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeviyImporter.Data
+{
+    public class MeviyImporterDBContext : DbContext
+    {
+
+        //パーツのDBテーブル
+        public DbSet<Part> Parts { get; set; }
+
+
+        public MeviyImporterDBContext()
+        {
+
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //SQLiteを使用するための宣言
+            optionsBuilder.UseSqlite("Data source=C:\\meviy\\meviy.db");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //テーブルとクラスの紐づけ
+            modelBuilder.Entity<Part>().ToTable("Parts");
+
+            //主キーにIdentityを設定
+            //SQLiteの場合はValueGeneratedOnAddを使用する。
+            modelBuilder.Entity<Part>()
+                .Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+        }
+    }
+}
---
+++ MeviyImporter/Model/Part.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeviyImporter.Model
+{
+    public class Part
+    {
+        [Key]
+        //主キー
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+        public string? Model { get; set; }
+        public string? Material { get; set; }
+        public string? SurfaceProcessing { get; set; }
+        public string? HeatTreatment { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+
+
+    }
+}
